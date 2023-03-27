ed35e0bbd59fd5346ef6e13073eb0e18acd8211e
Benedict <benedictryan80@gmail.com>
2023-03-27 16:03:52 +0700

    aaa

--- src/examples/HandleMultipleInputs.js
+++ src/examples/HandleMultipleInputs.js
@@ -15,7 +15,7 @@ function HandleMultipleInputs() {
 		<div>
 			<input type="text" name="name" onChange={handleChange} />
 			<input type="text" name="email" onChange={handleChange} />
-			<input type="number" name="age" onChange={handleChange} />{' '}
+			<input type="number" name="age" onChange={handleChange} />
 			<button onClick={() => console.log(user)}>Log on the console</button>
 		</div>
 	);
